0d6f3c3253e6f08cc4bf41dafbecf1b62da522cd
kishore951 <kishoresakthivel951@gmail.com>
2022-10-20 14:57:37 +0530

    okk

--- src/main/java/com/nestdigital/librarymanagement/Dao/LibraryDao.java
+++ src/main/java/com/nestdigital/librarymanagement/Dao/LibraryDao.java
@@ -1,7 +1,17 @@
 package com.nestdigital.librarymanagement.Dao;
 
 import com.nestdigital.librarymanagement.Model.LibraryModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LibraryDao extends CrudRepository<LibraryModel,Integer> {
+@Modifying
+    @Query(value = "DELETE FROM `library` WHERE `id` = :id",nativeQuery = true)
+    void deleteBookById(Integer id);
+
+@Query(value = "SELECT `id`, `author`, `book_title`, `description` FROM `library` WHERE `author`=:author",nativeQuery = true)
+List<LibraryModel> searchBook(String author);
 }
